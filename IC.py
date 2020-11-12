8ebab8e96311f10c878e21ff67b4fef74c054574
chinandy1991 <chinandy1991@gmail.com>
2020-11-13 05:21:48 +0800

    DIC cascade

--- IC.py
+++ IC.py
@@ -33,6 +33,65 @@ def runIC (G, S, p=0.01 ):
                  T.append(v)
     return T
 
+def runDIC (G, S, p=0.01, q=0.001 ):
+    ''' Runs independent cascade model.
+    Input: G -- networkx graph object
+    S -- initial list of vertices
+    V1 -- set of first attpemt influenced node
+    V2 -- set of second attpemt influenced node
+    p -- propagation probability
+    q -- 1 neighbor propagation probability
+    Output: T -- resulted influenced set of vertices (including S)
+    '''
+    T = deepcopy(S)
+    V1=[]
+    V2=[]
+    i = 0
+    for u in T: # T may increase size during iterations
+         for v in G[u]: # check whether new node v is influenced by chosen node u
+             #w = G[u][v]['weight']
+             w = 1
+             if v not in T:
+                 if v not in V1:
+                     if random.random() < q:
+                         T.append(v)
+                     else:
+                         V1.append(v)
+                 elif v in V1 and v not in V2:
+                     if random.random() < (1-(1-p**2)-q)/(1-q): #q+(1-q)x=(1-(1-p^2)), x=(1-(1-p^2)-q)/(1-q)
+                         T.append(v)
+                     else:
+                         V2.append(v) #with prob ()
+                 elif v in V2:
+                     if random.random() < p:
+                         T.append(v)
+    return T
+
+
+
+def runSC (G, S, d=1 ):
+    ''' Runs independent cascade model.
+    Input: G -- networkx graph object
+    S -- initial list of vertices
+    d -- propagation probability
+    Output: T -- resulted influenced set of vertices (including S)
+    '''
+    i = 0
+    while i < len(T):
+        for v in G[T[i]]: # for neighbors of a selected node
+            if v not in T: # if it wasn't selected yet
+                w = G[T[i]][v]['weight'] # count the number of edges between two nodes
+                if w==1:
+                    if random() <= 1 - (1-q)**w: # if at least one of edges propagate influence
+                        print T[i], 'influences', v
+                        T.append(v) 
+                else:
+                    if random() <= 1 - (1-p)**w: # if at least one of edges propagate influence
+                        print T[i], 'influences', v
+                        T.append(v)
+        i += 1
+    return T
+
 
 def runIC_repeat(G, S, p=0.01, sample=1000):
     infl_list = []
