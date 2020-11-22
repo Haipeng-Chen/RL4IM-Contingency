c3b42718d2d52095766ea944a8c86b97868eb54c
Haipeng Chen <haipeng@holylogin01.rc.fas.harvard.edu>
2020-11-21 22:12:30 -0500

    enable batch running

---
+++ Run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -n 1               # Number of cores (-n)
+#SBATCH -N 1                # Ensure that all cores are on one Node (-N)
+#SBATCH -t 0-0:30          # Runtime in D-HH:MM, minimum of 10 minutes
+#SBATCH -p serial_requeue   #tambe_gpu     # Partition to submit to
+#SBATCH --mem=5000           # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH -o output/%j.out  # File to which STDOUT will be written, %j inserts jobid
+#SBATCH --gres=gpu:1
+
+python rl4im.py --cascade 'DIC' --eps_decay True --eps_wstart 0.3 --propagate_p 0.3 --q 0.5 --use_cuda 0 
