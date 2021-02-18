074df5528a1262209e47970e156e2d53e3da3feb
GoingMyWay <scutqiuwei@163.com>
2021-02-18 14:40:12 +0800

    update

--- run_eval_real_graphs.sh
+++ run_eval_real_graphs.sh
@@ -8,16 +8,8 @@ do
         for bud in 1 2 4 8;
         do
             echo "start running method: ${method} on graph: ${g} with budget: ${bud}"
-            bash run_interactive.sh 3 python3.7 main.py --config=colge \
-                                                        --env-config=basic_env --results-dir=results/Feb18_RL4IM_real_graphs/${g}/${method} \
-                                                        with T=8 \
-                                                        budget=${bud} \
-                                                        save_every=2 \
-                                                        q=0.6 \
-                                                        mode='test' \
-                                                        realgraph=${g} \
-                                                        model_scheme=normal \
-                                                        checkpoint_path=model_ckpt/Feb17_RL4IM/colge/sacred/${bud}/models &
+            bash run_interactive.sh 3 python3.7 main.py --config=colge --env-config=basic_env --results-dir=results/Feb18_RL4IM_real_graphs/${g}/${method} with T=8 budget=${bud} save_every=2 q=0.6 mode='test' realgraph=${g} model_scheme=normal checkpoint_path=model_ckpt/Feb17_RL4IM/colge/sacred/${bud}/models &
+            sleep 3;
         done
     done
 done
