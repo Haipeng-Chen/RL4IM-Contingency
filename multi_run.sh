8863aa78419f6189fb171ba08d8f934e3c2b647e
Haipeng Chen <haipengkeepon@gmail.com>
2021-06-25 11:21:36 -0400

    Update multi_run.sh

--- multi_run.sh
+++ multi_run.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+## these commands are for slurm run, skip if you are not using slurm
+#SBATCH -n 1              # Number of cores (-n)
+#SBATCH -N 1                # Ensure that all cores are on one Node (-N)
+#SBATCH -t 0-12:00          # Runtime in D-HH:MM, minimum of 10 minutes
+#SBATCH -p Your_partition # Partition to submit to
+#SBATCH --mem=10000           # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH --gres=gpu:1
+#SBATCH -o output/%j.out  # File to which STDOUT will be written, %j inserts jobid
 
 ### THE FOLLOWING IS FOR SELECTING WHICH WAY TO RUN YOUR TASKS
 # Read command line options
