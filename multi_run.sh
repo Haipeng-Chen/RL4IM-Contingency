97b5753200da17fe337481fa2d2c5277ab8cf143
w821881341 <w821881341@gmail.com>
2021-06-25 22:36:32 +0800

    override arguments

--- multi_run.sh
+++ multi_run.sh
@@ -105,11 +105,11 @@ BASE_CMD="--Q $Q \
           --gpuid ${GPU_ID}"
 
 #### RL training
-# Q1: ablation study of reward shaping and state-abstraction
+Q1: ablation study of reward shaping and state-abstraction
 RL_TRAIN_CMD_ABL_ALL="${BASE_CMD} --USE_STATE_ABS $USE_STATE_ABS"
 for REWARD_TYPE  in 0 1 3
 do  
-    ${runner} gpu_run.sh ${RL_TRAIN_CMD_ABL_ALL}; sleep 20;
+    ${runner} gpu_run.sh ${RL_TRAIN_CMD_ABL_ALL} --REWARD_TYPE ${REWARD_TYPE}; sleep 20;
 done
 #
 USE_STATE_ABS=False
@@ -122,21 +122,21 @@ USE_STATE_ABS=True
 RL_TRAIN_CMD_Q2="${BASE_CMD} --USE_STATE_ABS $USE_STATE_ABS"
 for GRAPH_NBR_TRAIN in 10 50 100 200 500
 do
-    ${runner} gpu_run.sh ${RL_TRAIN_CMD_Q2}; sleep 20;
+    ${runner} gpu_run.sh ${RL_TRAIN_CMD_Q2} --GRAPH_NBR_TRAIN ${GRAPH_NBR_TRAIN}; sleep 20;
 done
 
 # Q3: vary q
 RL_TRAIN_CMD_Q3="${BASE_CMD} --USE_STATE_ABS $USE_STATE_ABS"
 for Q in 0.2 0.4 0.6 0.8 
 do
-    ${runner} gpu_run.sh ${RL_TRAIN_CMD_Q3}; sleep 20;
+    ${runner} gpu_run.sh ${RL_TRAIN_CMD_Q3} --Q ${Q}; sleep 3;
 done
 
 # Q4: vary T = 8, 4, 2, 1 (note that T in the paper is number of time steps, in the code T is number of time steps*budget)
 RL_TRAIN_CMD_Q4="${BASE_CMD} --USE_STATE_ABS $USE_STATE_ABS"
 for BUDGET in 1 2 4 8
 do
-    ${runner} gpu_run.sh ${RL_TRAIN_CMD_Q4}; sleep 20;
+    ${runner} gpu_run.sh ${RL_TRAIN_CMD_Q4} --BUDGET ${BUDGET}; sleep 20;
 done
 
 # Q5: vary |V|
@@ -144,27 +144,24 @@ RL_TRAIN_CMD_Q5="${BASE_CMD} --USE_STATE_ABS $USE_STATE_ABS"
 for NODE_TRAIN in 50 100 200 500 1000
 do
     NODE_TEST=NODE_TRAIN
-    ${runner} gpu_run.sh ${RL_TRAIN_CMD_Q5}; sleep 20;
+    ${runner} gpu_run.sh ${RL_TRAIN_CMD_Q5} --NODE_TEST ${NODE_TEST} --NODE_TRAIN ${NODE_TRAIN}; sleep 20;
 done
 
-#### RL test, an example when q=0.2
+# #### RL test, an example when q=0.2
 MODE='test'
 CHECK_POINT_PATH=./results/rl4im/sacred/113/models
 LOAD_STEP=1580
 Q=0.2
 RL_TRAIN_CMD_TEST="${BASE_CMD} --USE_STATE_ABS $USE_STATE_ABS --CHECK_POINT_PATH $CHECK_POINT_PATH --LOAD_STEP $LOAD_STEP"
-${runner} gpu_run.sh ${RL_TRAIN_CMD_TEST}; sleep 20;
+${runner} gpu_run.sh ${RL_TRAIN_CMD_TEST} --MODE ${MODE}; sleep 20;
 
-#### CHANGE/random, an example of Question 3
+# #### CHANGE/random, an example of Question 3
 MODE='test'
 RL_TRAIN_CMD_TEST2="${BASE_CMD} --USE_STATE_ABS $USE_STATE_ABS"
 for METHOD in 'lazy_adaptive_greedy' 'random'
 do
 for Q in 0.2 0.4 0.6 0.8 
 do
-    ${runner} gpu_run.sh ${RL_TRAIN_CMD_TEST2}; sleep 20;
+    ${runner} gpu_run.sh ${RL_TRAIN_CMD_TEST2} --MODE ${MODE} --METHOD ${METHOD} --Q ${Q}; sleep 20;
 done
 done
-
-
-
